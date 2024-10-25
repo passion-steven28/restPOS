516298706d5f6468c142cf04fc8fd9c6aa5060dd
passion studio <120102511+passion-steven28@users.noreply.github.com>
2024-10-25 05:59:58 +0300

    creating views

--- django/api/views.py
+++ django/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Product, Category, Order, OrderItem, Reservation, UserProfile
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .serializers import ProductSerializer, CategorySerializer, OrderSerializer, OrderItemSerializer, ReservationSerializer, UserProfileSerializer
 
 # Create your views here.
+class ProductList(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryList(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class OrderList(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemList(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+class ReservationList(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+class UserProfileList(viewsets.ModelViewSet):
+    # include the UserProfileSerializer in the response
+    queryset = UserProfile.objects.all() 
+    serializer_class = UserProfileSerializer
